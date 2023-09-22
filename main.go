01c6c1fe09e979f170d222ebee0dec4eb1535ba5
Andreea Daniela Burnaz <danielaburnaz@gmail.com>
2023-09-22 12:00:06 +0300

    edit main.go

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"io/ioutil"
 )
 
 const serverPort = 8080
@@ -16,4 +18,29 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+	GetApi()
+}
+func GetApi() {
+    // url := "https://groupietrackers.herokuapp.com/api/artists"
+    // req, err := http.NewRequest("GET", url, nil)
+    // if err != nil {
+    //     fmt.Print(err.Error())
+    // }
+	// res, err := http.DefaultClient.Do(req)
+	// if err != nil {
+    //     fmt.Print(err.Error())
+    // }
+	// defer res.Body.Close()
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+
+    if err != nil {
+        fmt.Print(err.Error())
+        os.Exit(1)
+    }
+
+    responseData, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(string(responseData))
 }
