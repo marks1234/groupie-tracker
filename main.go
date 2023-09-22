4d62737311ed5c9dd5a2148cb935b069e7ae2bbe
Andreea Daniela Burnaz <danielaburnaz@gmail.com>
2023-09-22 12:14:04 +0300

    created artists struct

--- main.go
+++ main.go
@@ -10,6 +10,42 @@ import (
 
 const serverPort = 8080
 
+type Response struct {
+	Id		int		  `json:"id"`
+	Image 	string	  `json:"image"`
+    Name    string    `json:"name"`
+	Members []string  `json:"members"`
+	CreationDate int  `json:"creationDate"`
+	FirstAlbum string `json:"firstAlbum"`
+}
+
+
+
+func GetApi() {
+	// url := "https://groupietrackers.herokuapp.com/api/artists"
+	// req, err := http.NewRequest("GET", url, nil)
+	// if err != nil {
+	//     fmt.Print(err.Error())
+	// }
+	// res, err := http.DefaultClient.Do(req)
+	// if err != nil {
+	//     fmt.Print(err.Error())
+	// }
+	// defer res.Body.Close()
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
@@ -19,28 +55,4 @@ func main() {
 		log.Fatal(err)
 	}
 	GetApi()
-}
-func GetApi() {
-    // url := "https://groupietrackers.herokuapp.com/api/artists"
-    // req, err := http.NewRequest("GET", url, nil)
-    // if err != nil {
-    //     fmt.Print(err.Error())
-    // }
-	// res, err := http.DefaultClient.Do(req)
-	// if err != nil {
-    //     fmt.Print(err.Error())
-    // }
-	// defer res.Body.Close()
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/")
-
-    if err != nil {
-        fmt.Print(err.Error())
-        os.Exit(1)
-    }
-
-    responseData, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(string(responseData))
 }
