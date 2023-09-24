31ba01ce8ff43ec9bca69a4e1af441a73be1f225
mkuzmina <marcokuzmina@gmail.com>
2023-09-24 19:50:56 +0300

    lol broken, life is over

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"regexp"
 	// "html/template"
 )
+
+type Locations struct{}
 
 type Response struct {
 	Id           int      `json:"id"`
@@ -20,9 +20,14 @@ type Response struct {
 	CreationDate int      `json:"creationDate"`
 	FirstAlbum   string   `json:"firstAlbum"`
 	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
 }
 
-var tmpl = template.Must(template.ParseFiles("index.html"))
+var (
+	tmpl        = template.Must(template.ParseFiles("index.html"))
+	tmpl_gotcha = template.Must(template.ParseFiles("gotcha.html"))
+)
 
 func GetApi() []Response {
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
@@ -30,25 +35,27 @@ func GetApi() []Response {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
+
 	defer response.Body.Close()
 	// fmt.Println(response.Body)
-	responseData, _ := ioutil.ReadAll(response.Body)
 
 	// var bands []BandInfo = string(responseData)
 	var responseObjects []Response
-
-	re := regexp.MustCompile(`{.+?}`)
-	str_arr := re.FindAllString(string(responseData), -1)
-	for _, str := range str_arr {
-		var responseObject Response
-		json.Unmarshal([]byte(str), &responseObject)
-		responseObjects = append(responseObjects, responseObject)
+	if err := json.NewDecoder(response.Body).Decode(&responseObjects); err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
 	}
-	fmt.Println(responseObjects)
+
+	// fmt.Println(responseObjects)
 	return responseObjects
 }
 
 func BandsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	// Call the GetApi function to fetch the data
@@ -59,11 +66,26 @@ func BandsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func PrivateHandler(w http.ResponseWriter, r *http.Request, res Response) {
+	if err := tmpl_gotcha.ExecuteTemplate(w, "gotcha.html", []Response{res}); err != nil {
+		http.Error(w, "Error executing template", http.StatusInternalServerError)
+	}
+}
+
 func main() {
 	// http.HandleFunc("/artists", BandsHandler)
 
 	// fileServer := http.FileServer(http.Dir("./static"))
+	// fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/static", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/", BandsHandler)
+	for _, res := range GetApi() {
+		res := res
+		http.HandleFunc(fmt.Sprint("/", res.Name), func(w http.ResponseWriter, r *http.Request) {
+			PrivateHandler(w, r, res)
+		})
+	}
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
